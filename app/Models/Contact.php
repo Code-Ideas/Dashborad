f90d25eb5f03e90871065973557bbb656ce7f8ea
Ahmed Gad <ahmed.ali-gad@outlook.com>
2021-01-19 14:55:50 +0200

    Add Contact Model & Migration

---
+++ app/Models/Contact.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $fillable = ['name', 'phone', 'email', 'message'];
+}
