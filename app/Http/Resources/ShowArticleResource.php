651ccddf057b2164eea0dbfef966993d367f8ee5
Ahmed Gad <ahmed.ali-gad@outlook.com>
2021-01-20 20:43:54 +0200

    Add Articles List & Show Article Resources

---
+++ app/Http/Resources/ShowArticleResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowArticleResource extends ArticleListResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return array_merge(parent::toArray($request), [
+            'content' => $this->content,
+            'section_id' => $this->section_id
+        ]);
+    }
+}
